a0a611ab022e297571adf2786e500b0f614d618b
Ma9ku <93972262+Ma9ku@users.noreply.github.com>
2023-04-27 22:49:29 +0600

    first-commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# nomad_player
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/nomad_player/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.nomad_player
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
